b5dbfef7aa2aebe423ae5054aac1371d6c18d6ac
Darshan Raju <darshanr163@gmail.com>
2021-09-26 22:19:08 +0530

    Fix schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -17,7 +17,7 @@ CREATE UNIQUE INDEX email_unique_index ON users.accounts USING btree (lower((ema
 CREATE UNIQUE INDEX user_name_unique_index ON users.accounts USING btree (lower((user_name)::text));
 
 insert into users.accounts(user_name,full_name,email,password)
-values ('admin','admin','admin@dekk.in','59918edb2105cf65df7b59cd0127491a')
+values ('admin','admin','admin@dekk.in',md5('welcometothejungle'));
 
 CREATE TABLE user_content.cards (
 	card_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -32,17 +32,38 @@ CREATE TABLE user_content.cards (
 	"permission" varchar NULL,
 	"type" varchar NULL,
 	image_links jsonb NULL,
+	card_hash varchar null,
 	CONSTRAINT fk_card_and_user FOREIGN KEY (account_id) REFERENCES users.accounts(account_id)
 );
 
-CREATE TABLE user_content.category (
-	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE user_content.tags (
+	tag_id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	created_by_id int4 NULL,
 	created_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
 	field varchar NULL,
 	category_name varchar NULL,
-	priority int4 NULL,
-	parent_topic int4 NULL,
-	created_by_id int4 NULL,
-	CONSTRAINT pk_dekk_id PRIMARY KEY (id)
+	is_master_topic boolean,
+	CONSTRAINT pk_dekk_id PRIMARY KEY (tag_id),
+	CONSTRAINT fk_tag_and_user FOREIGN KEY (created_by_id) REFERENCES users.accounts(account_id)
 );
+
+
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','infectious-diseases',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','cardiovascular-system',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','respiratory-system',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','kidney-and-urinary-tract',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','rheumatology-and-immunology',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','neurology',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','endocrinology',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','biochemistry',true,1);
+insert into user_content.tags (field,category_name,is_master_topic,created_by_id) values
+('Medical','hemat-oncology',true,1);
