73b233b608ea8ea3ca9e6c73f55bafc976ca30ca
Srinidhi S <srinidhi.nps@gmail.com>
2021-12-19 22:07:53 +0530

    missed files for prev commit

--- frontend/src/app/pages/text-edit/text-edit.component.ts
+++ frontend/src/app/pages/text-edit/text-edit.component.ts
@@ -1,25 +1,85 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { EditorComponent } from '@tinymce/tinymce-angular';
 // import { Editor, toDoc } from 'ngx-editor';
 import { toDoc } from 'ngx-editor';
+// import { EventEmitter } from 'stream';
 
+import "tinymce/themes/modern";
+import "tinymce/plugins/colorpicker";
+import "tinymce";
+import { CardUtils } from 'src/app/common/utils/card.utils';
+declare var tinymce: any;
 
 @Component({
   selector: 'app-text-edit',
   templateUrl: './text-edit.component.html',
   styleUrls: ['./text-edit.component.scss']
 })
-export class TextEditComponent implements OnInit, OnDestroy {
+export class TextEditComponent implements OnInit, OnDestroy, AfterViewInit {
 
+  @Input()
   content = '';
+
+  @Input() 
+  elementId: string;
+
+  editor: any;
 
   constructor() {}
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      tinymce.init({
+        selector: '#' + this.elementId,
+        menubar: false,
+        plugins: "textcolor colorpicker",
+        toolbar: "backcolor",
+        textcolor_cols: "1",
+        textcolor_rows: "1",
+        custom_colors: false,
+        textcolor_map: [
+          CardUtils.DEFAULT_TEXT_HIGHLIGHT_COLOR.slice(1)
+        ],
+        force_br_newlines : true,
+        force_p_newlines : false,
+        forced_root_block : '',
+        setup: (editor: any) => {
+          editor.on('init', (e: any) => {
+            editor.setContent(this.content);
+          });
+        }
+      });
+  
+      tinymce.get('#' + this.elementId).setContent(this.content); 
+    }, 1000);
+  }
+
   ngOnInit(): void {}
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+  }
 
-  checkModel() {
-    console.log(toDoc(this.content));
-    // console.log(this.content);
+  constructCardContent(): string {
+    return '';
+  }
+
+  convertCardContentToPayload(): string {
+    let processedText = '';
+    toDoc(tinymce.get(this.elementId).getContent())
+      ?.content[0]
+      ?.content
+      ?.forEach((contentComponent: any) => {
+        if (contentComponent?.type === 'text') {
+          if (contentComponent.marks) {
+            processedText += '*' + contentComponent.text + '*';
+          } else {
+            processedText += contentComponent.text;
+          }
+        } else if (contentComponent.type === 'hard_break') {
+          processedText += '\n';
+        }
+      }
+    );
+    return processedText;
   }
 }
