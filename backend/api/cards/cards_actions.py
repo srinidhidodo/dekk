f2f3689706a78d4679933af59fae6da5f0b853a6
random-raju <darshanr163@gmail.com>
2022-01-03 00:00:45 +0530

    Add edit api and minor changes

--- backend/api/cards/cards_actions.py
+++ backend/api/cards/cards_actions.py
@@ -17,6 +17,7 @@ import re
 import falcon
 import jwt
 from api.user import authorization
+from deepdiff import DeepDiff
 from psycopg2.errors import ForeignKeyViolation
 from psycopg2.errors import UniqueViolation
 from utils import http_response
@@ -28,6 +29,37 @@ HASH_KEYS_CARDS = [
     "content_on_back",
     "account_id",
 ]
+
+
+def card_difference(og_card, current_card):
+
+    og_card.pop("highlighted_keywords", None)
+    og_card.pop("permission", None)
+    og_card.pop("type", None)
+    og_card.pop("type", None)
+    og_card.pop("image_links", None)
+    og_card.pop("card_id", None)
+
+    difference = DeepDiff(og_card, current_card)
+    updated_fields = []
+
+    for key in difference:
+        for col_key in difference[key].keys():
+            updated_fields.append((col_key.split("['")[1].split("']")[0]))
+
+    return updated_fields
+
+
+def tags_difference(og_tags, current_tags):
+
+    difference = DeepDiff(og_tags, current_tags)
+    updated_fields = []
+
+    for key in difference:
+        for col_key in difference[key].keys():
+            updated_fields.append((col_key.split("['")[1].split("']")[0]))
+
+    return updated_fields
 
 
 def get_hash_for_cards(data_dict, hsh_keys):
@@ -75,7 +107,7 @@ def get_hash_for_tags(data_dict):
 def get_all_card_ids_for_a_dekk(db_conn, tag_id):
 
     card_ids_query = f"""
-        SELECT t1.title,t1.card_id FROM user_content.cards t1 inner join user_content.tags_cards t2
+        SELECT t1.title,t1.card_id,t1.content_on_front FROM user_content.cards t1 inner join user_content.tags_cards t2
         on t1.card_id = t2.card_id
         where t2.tag_id = '{tag_id}'
         order by t1.title
@@ -118,6 +150,18 @@ def get_card_by_id(db_conn, card_id):
          FROM user_content.cards
         where card_id = '{card_id}'
         limit 1
+    """
+    result = db_conn.fetch_query_direct_query(card_ids_query)
+
+    return result
+
+
+def get_card_and_its_tags(db_conn, card_id):
+
+    card_ids_query = f"""
+        select t1.tag_id,t2.is_master_topic,t2.tag_type from user_content.tags_cards t1 inner join user_content.tags t2 on
+        t1.tag_id = t2.tag_id and
+        t1.card_id = '{card_id}'
     """
     result = db_conn.fetch_query_direct_query(card_ids_query)
 
@@ -336,6 +380,182 @@ def create_dekk(db_conn, req):
         raise falcon.HTTPBadRequest("Oops something when wrong", message)
 
 
+def edit_card(db_conn, req):
+
+    req_data = req.media
+
+    req_data = req_data
+
+    if not req_data:
+        return {}
+
+    card_dict = {}
+
+    env = os.environ.get(f"ENV")
+    secret = os.environ.get(f"SECRET_{env}")
+    token = req.headers.get("AUTHORIZATION")
+    decode = jwt.decode(token, secret, verify="False", algorithms=["HS256"])
+
+    if not req_data.get("title", ""):
+        error_message = "Something went wrong , seems like the tittle is empty"
+        message = {"message": error_message}
+        raise falcon.HTTPBadRequest("Oops something when wrong", message)
+
+    if "new_tags" not in req_data:
+        error_message = "Something went wrong , seems new_tags is not in request body"
+        message = {"message": error_message}
+        raise falcon.HTTPBadRequest("Oops something when wrong", message)
+
+    if "selected_tag_ids" not in req_data:
+        error_message = (
+            "Something went wrong , seems selected_tag_ids is not in request body"
+        )
+        message = {"message": error_message}
+        raise falcon.HTTPBadRequest("Oops something when wrong", message)
+
+    if "dekk_id" not in req_data:
+        error_message = "Something went wrong , seems dekk_id is not in request body"
+        message = {"message": error_message}
+        raise falcon.HTTPBadRequest("Oops something when wrong", message)
+
+    if "content_on_front" not in req_data:
+        error_message = (
+            "Something went wrong , seems content_on_front is not in request body"
+        )
+        message = {"message": error_message}
+        raise falcon.HTTPBadRequest("Oops something when wrong", message)
+
+    if "content_on_back" not in req_data:
+        error_message = (
+            "Something went wrong , seems content_on_back is not in request body"
+        )
+        message = {"message": error_message}
+        raise falcon.HTTPBadRequest("Oops something when wrong", message)
+
+    if "card_id" not in req_data:
+        error_message = "Something went wrong , seems card_id is not in request body"
+        message = {"message": error_message}
+        raise falcon.HTTPBadRequest("Oops something when wrong", message)
+
+    card_dict["title"] = req_data["title"]
+    card_dict["content_on_front"] = req_data["content_on_front"]
+    card_dict["content_on_back"] = req_data["content_on_back"]
+
+    selected_tag_ids = req_data.pop("selected_tag_ids", [])
+    dekk_id = req_data.pop("dekk_id", None)
+    new_tags = req_data.pop("new_tags", [])
+    card_id = req_data.pop("card_id", None)
+
+    for_search = (
+        card_dict["title"]
+        + " "
+        + card_dict["content_on_front"]
+        + " "
+        + card_dict["content_on_back"]
+    )
+    for_search = re.sub(r"\s{2,}", " ", for_search).strip()
+    for_search = re.sub(r"_", " ", for_search).strip()
+    for_search = re.sub(r"\W", " ", for_search).strip()
+    for_search = re.sub(r"\r", " ", for_search).strip().lower()
+
+    # compare cards
+    og_card = get_card_by_id(db_conn, card_id)
+    updated_card_fields = card_difference(og_card[0], card_dict)
+
+    card_dict["account_id"] = decode["account_id"]
+
+    card_dict["for_search"] = re.sub(r"\s{2,}", " ", for_search).strip().lower()
+    card_dict["card_id"] = card_id
+
+    # get updated fields
+    if updated_card_fields:
+        updated_card = {}
+        for key in updated_card_fields:
+            updated_card[key] = card_dict[key]
+
+        updated_card["card_id"] = card_id
+    else:
+        updated_card = {}
+
+    # compare tags
+    tags = get_card_and_its_tags(db_conn, card_id)
+    og_tags = [tag["tag_id"] for tag in tags]
+
+    edited_new_tags = []  # to be inserted to user_content.tags
+
+    for tag in new_tags:
+        dict_ = {
+            "tag_name": tag.strip(),
+            "account_id": card_dict["account_id"],
+        }
+        dict_["tag_id"] = get_hash_for_tags(dict_)
+        dict_["tag_type"] = "tag"
+        dict_["parent_topic_hash"] = dekk_id
+
+        edited_new_tags.append(dict_)
+
+    tags_cards = []  # to be inserted to user_content.tags_cards
+    for tag in edited_new_tags:
+        dict_ = {"card_id": card_dict["card_id"], "tag_id": tag["tag_id"]}
+        tags_cards.append(dict_)
+
+    dekk_dict = {"card_id": card_dict["card_id"], "tag_id": dekk_id}
+    tags_cards.append(dekk_dict)
+
+    for tag in selected_tag_ids:
+        dict_ = {"card_id": card_dict["card_id"], "tag_id": tag}
+        tags_cards.append(dict_)
+
+    for tag in edited_new_tags:  # add new tags to tags
+        try:
+            db_conn.table = "tags"
+            status = db_conn.pg_handle_insert(tag)
+            if status == 0:
+                raise Exception("Oops something went wrong could not create new tag")
+        except UniqueViolation as e:
+            # print(e)
+            pass
+        except Exception as e:
+            raise e
+
+    try:
+        curosr = db_conn.conn_obj.cursor
+        curosr.execute("BEGIN")
+        curosr.execute(
+            "DELETE FROM user_content.tags_cards WHERE card_id = %s;", (card_id,)
+        )
+        try:  # add card
+            if updated_card:
+                db_conn.table = "cards"
+                status = status = db_conn.pg_handle_update(
+                    updated_card, "card_id", commit=False
+                )
+            else:
+                pass  # no changes to card edit only tags
+        except Exception as e:
+            print(e)
+            raise e
+
+        for tags_card in tags_cards:  # add tags_cards
+            try:
+                db_conn.table = "tags_cards"
+                status = db_conn.pg_handle_insert(tags_card, commit=False)
+            except UniqueViolation as e:
+                print(e)
+                pass
+            except Exception as e:
+                raise e
+        curosr.execute("COMMIT")
+    except Exception as e:
+        curosr.execute("ROLLBACK")
+        print(e)
+        raise Exception(
+            "Oops something went wrong could not create new card - looks like same card already exists"
+        )
+
+    return True
+
+
 class CreateDekk:
     """
         Request data has to be a json
@@ -363,9 +583,18 @@ class CreateCard:
 
     @falcon.before(authorization.request_valiation)
     def on_post(self, req, resp):
-        print(req)
         try:
             create_card(self.db_conn, req)
+            http_response.ok(resp, {"message": "Created the card"})
+        except Exception as e:
+            error_message = str(e)
+            message = {"message": error_message}
+            http_response.err(resp, falcon.HTTP_500, message)
+
+    @falcon.before(authorization.request_valiation)
+    def on_put(self, req, resp):
+        try:
+            edit_card(self.db_conn, req)
             http_response.ok(resp, {"message": "Created the card"})
         except Exception as e:
             error_message = str(e)
@@ -408,7 +637,6 @@ class GetCardById:
 
     @falcon.before(authorization.request_valiation)
     def on_get(self, req, resp, card_id):
-        print(card_id)
         query_result = get_card_by_id(self.db_conn, card_id)
 
         if card_id:  # dekk id is tag id
--- backend/utils/postgres.py
+++ backend/utils/postgres.py
@@ -116,7 +116,6 @@ class QueryManager:
         """
         try:
             op = self.conn_obj.cursor.execute(skeleton, data_dict)
-            print(op)
             session_id = self.conn_obj.cursor.fetchone()[0]
         except Exception as e:
             traceback.print_exc()
@@ -183,6 +182,70 @@ class QueryManager:
             if commit:
                 self.conn_obj.conn.commit()
 
+        return self.conn_obj.cursor.rowcount
+
+    def pg_handle_update(self, data_dict, unique_key, commit=True):
+        """
+        A function like this is being made with the goal
+        to have one agreed upon function for all Postgres updates.
+        Important pre-requisites:
+            - The input is always a python dictionary
+            -
+        Params:
+            - conn: Psycopg2 connection object
+            - cur: Psycopg2 cursor object
+            - collection: Name of the Postgres collection
+            - table: Name of the table
+            - data_dict: The updated raw in the form of a dictionary
+            - update: Whether to actually make the update or not
+            - append: When you want to append to a value already there in the column
+        """
+        # Getting the raw hash but ensuring raw hash isn't in the
+        # update statement to avoid the hsh_unique constraint conflict
+        unique_key_val = data_dict[unique_key]
+        data_dict.pop(unique_key)
+        columns = data_dict.keys()
+        # See https://github.com/mozilla/http-observatory/issues/298
+        # for why this IF condition is needed
+        if len(columns) > 1:
+            skel_columns = f"({', '.join(columns)})"
+            skel_percentized_columns = [f"%({col})s" for col in columns]
+            skel_values = f"({', '.join(skel_percentized_columns)})"
+            skeleton = f"""
+                    UPDATE {self.collection}.{self.table}
+                    SET {skel_columns} =
+                        {skel_values}
+                    WHERE {unique_key} = '{unique_key_val}'
+                        """
+        elif len(columns) == 1:
+            skel_column = list(columns)[0]
+            skel_value = f"%({skel_column})s"
+            skeleton = f"""
+                    UPDATE {self.collection}.{self.table}
+                    SET {skel_column} =
+                        {skel_value}
+                    WHERE {unique_key} = '{unique_key_val}'
+                        """
+
+        """
+        The following is where the real utility should lie (hopefully).
+        We simply pass a dictionary here not worried about any
+        Postgres statement implementation and let Psycopg2 handle it.
+        No tuples, nothing, just a plain old data_dict.
+        """
+        try:
+            self.conn_obj.cursor.execute(skeleton, data_dict)
+        except Exception as e:
+            traceback.print_exc()
+            self.conn_obj.conn.rollback()
+            """
+            This helper function will actually raise
+            an exception and let what's calling it take
+            care of what to do
+            """
+            raise e
+        if commit:
+            self.conn_obj.conn.commit()
         return self.conn_obj.cursor.rowcount
 
     def pg_index_search_text(self):
