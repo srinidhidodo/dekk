08ff21fdf556a0652abd58523b2a309d90ea9e1c
Srinidhi S <srinidhi.nps@gmail.com>
2022-01-02 18:48:26 +0530

    fix auto login based on one day expiry

--- frontend/src/app/common/services/user.service.ts
+++ frontend/src/app/common/services/user.service.ts
@@ -6,17 +6,21 @@ import { Injectable } from "@angular/core";
 export class UserService {
     accessToken: string = '';
     loggedIn: boolean = false;
+    EXPIRY_IN_MS: number = 86400000;
 
     initializeWithPreviousLogin(): void {
-        if (localStorage['accessToken']) {
+        if (localStorage['accessToken'] && !!(localStorage['expiry']) && localStorage['expiry'] > new Date().getTime()) {
             this.loggedIn = true;
             this.accessToken = localStorage['accessToken'];
+        } else {
+            this.logout();
         }
     }
 
     loginSuccessful(accessToken: string): void {
         this.accessToken = accessToken;
         localStorage['accessToken'] = accessToken;
+        localStorage['expiry'] = new Date().getTime() + this.EXPIRY_IN_MS;
         this.loggedIn = true;
     }
 
@@ -24,5 +28,6 @@ export class UserService {
         this.accessToken = '';
         this.loggedIn = false;
         localStorage.removeItem('accessToken');
+        localStorage.removeItem('expiry');
     }
 }
